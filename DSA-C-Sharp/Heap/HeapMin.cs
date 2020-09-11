f198e3efb099c56b534744c941e461bedb77c573
Tevin <tevinanthony95@gmail.com>
2020-09-11 14:38:51 -0500

    Heap Pop

--- DSA-C-Sharp/Heap/HeapMin.cs
+++ DSA-C-Sharp/Heap/HeapMin.cs
@@ -5,7 +5,11 @@ namespace DSA_C_Sharp.Heap {
     public class HeapMin<T> where T: IComparable<T> {
 
         DynamicArray<T> collection;
-        public int Count { get; set; }
+        public int Count { get=> collection.Length;}
+
+        public bool IsEmpty {
+            get => collection.IsEmpty();
+        }
 
         public HeapMin(int capacity) {
             if (capacity < 0)
@@ -85,6 +89,30 @@ namespace DSA_C_Sharp.Heap {
             }
         }
 
+        private void SiftDown() {
+            int index = 0;
+
+            while (HasLeft(index) || HasRight(index)) {
+                int originalIndex = index;
+                int left = GetLeft(index);
+                int right = GetRight(index);
+
+                //If the new root node is greater than the left child node
+                if (HasLeft(originalIndex) && collection[index].CompareTo(collection[left]) == 1)
+                    index = left;
+
+                //If the right node is smaller than the current index
+                if (HasRight(originalIndex) && collection[index].CompareTo(collection[right]) == 1)
+                    index = right;
+
+                if (index != originalIndex) {
+                    T temp = collection[originalIndex];
+                    collection[originalIndex] = collection[index];
+                    collection[index] = temp;
+                }
+            }
+        }
+
         /// <summary>
         /// Adds some data to the heap.
         /// </summary>
@@ -105,7 +133,31 @@ namespace DSA_C_Sharp.Heap {
             throw new Exception("The heap is currently empty.");
         }
 
+        /// <summary>
+        /// Returns the root node.
+        /// </summary>
+        /// <returns></returns>
+        public T Pop() {
+            if (collection.Length < 0)
+                throw new Exception("No root node found. The heap is empty.");
 
+            T result = collection[0];
+            collection[0] = collection[collection.Length - 1];
+            collection.RemoveLast();
+            SiftDown();
+
+            return result;
+        }
+
+        /// <summary>
+        /// Remove all elements from the heap.
+        /// </summary>
+        public void Clear() {
+            if (collection.IsEmpty())
+                throw new Exception("Heap is empty.");
+
+            collection.Clear();
+        }
 
     }
 }
